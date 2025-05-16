0b7d72d28a06f3240da6aa1e322dcdd3b7f9c3cd
Robert <robstef@protonmail.com>
2025-05-16 19:20:15 +0200

    dodany sinus i cosinus

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <matplot/matplot.h>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+namespace mp = matplot;
+void cosinus(double amplituda, double czestotliwosc, double t_min, double t_max, double samples){
+vector<double> wynik;
+//wynik.resize(samples);
+vector<double> os_x = mp::linspace(t_min, t_max, samples);
+for(int i=0; i<samples;i++){
+cout<< setw(5)<< os_x[i]<<endl;
+}
+for (int i = 0; i<samples; i++){
+wynik.push_back(amplituda*cos(2*M_PI*os_x[i]*czestotliwosc));
+
+}
+mp::plot(os_x, wynik);
+mp::show();
+}
+void sinus(double amplituda, double czestotliwosc, double t_min, double t_max, double samples){
+vector<double> wynik;
+//wynik.resize(samples);
+vector<double> os_x = mp::linspace(t_min, t_max, samples);
+for(int i=0; i<samples;i++){
+cout<< setw(5)<< os_x[i]<<endl;
+}
+for (int i = 0; i<samples; i++){
+wynik.push_back(amplituda*sin(2*M_PI*os_x[i]*czestotliwosc));
+
+}
+for(int i=0; i<samples;i++){
+//cout<< setw(5)<< os_x[i];
+//cout<<wynik[i]<<endl;
+}
+mp::plot(os_x, wynik);
+mp::show();
+}
+int main(){
+cosinus(2,0.55,0,50,300);
+return 0;
+}
